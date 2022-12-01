128ead12e4fa107581c36ba5859ed85c88eb6dab
edmundspira <edmunddspira@gmail.com>
2022-12-01 12:25:58 -0800

    task 5 update

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
-def main():
-    import hidden_4
-    for i in dir(hidden_4):
-        if ("__" not in i):
-            print(i)
 
-if (__name__ == "__main__"):
-    main()
+# Prints out all the names defined by the given compiled module below
+def displayNames():
+    hidden_4 = __import__("hidden_4")
+
+    defNames = dir(hidden_4)
+    for name in defNames:
+        if name[:2] not in "__":
+            print(name)
+
+
+if __name__ == "__main__":
+    displayNames()
