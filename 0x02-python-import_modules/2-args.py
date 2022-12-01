908635daa40d41f05704315a1aa7f0160f779941
edmundspira <edmunddspira@gmail.com>
2022-12-01 12:14:26 -0800

    task 3 new update

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-
-    num_of_args = len(sys.argv) - 1
-
-    if num_of_args == 0:
-        print("{} arguments.".format(num_of_args))
-    elif num_of_args == 1:
-        print("{} argument:".format(num_of_args))
-        print("{}: {}".format(num_of_args, sys.argv[1]))
-    else:
-        print("{} arguments:".format(num_of_args))
-        # loop through the list and print
-        for i in range(num_of_args + 1):
-            if i != 0:
-                print("{}: {}".format(i, sys.argv[i]))
+from sys import argv
+__name__ == "__main__"
+if len(argv) == 1:
+    print("0 arguments.")
+else:
+    s = "arguments"
+    print("{} {}:".format(len(argv) - 1, s[:-1] if len(argv) == 2 else s))
+    for x in range(1, len(argv)):
+        print("{}: {}".format(x, argv[x]))
