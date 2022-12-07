d3b1cb5e835bc7c9160b7f7fbc95738c695a4fad
edmundspira <edmunddspira@gmail.com>
2022-12-07 05:39:20 -0800

    Task 1 update

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -6,11 +6,3 @@ def square_matrix_simple(matrix=[]):
         matrix1.append(list(row))
 
     return matrix1
-
-matrix = [
-        [1, 2, 3],
-        [4, 5, 6],
-        [7, 8, 9]]
-matrix1 = square_matrix_simple(matrix)
-print(matrix1)
-print(matrix)
