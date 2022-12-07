a9b8e5e37c3c0ec130ad9a518862cc5944f5da18
edmundspira <edmunddspira@gmail.com>
2022-12-07 05:31:20 -0800

    Task 16 update

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def complex_delete(a_dictionary, value):
+def complex_delete(my_dict, value):
     if value not in my_dict.values() or my_dict == {}:
         return (my_dict)
     dele = [x for x in my_dict if my_dict[x] == value]
