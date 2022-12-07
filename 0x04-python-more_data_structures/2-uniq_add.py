aa40d0ef87a1c741e7a17e2d1532c3c62cbc5928
edmundspira <edmunddspira@gmail.com>
2022-12-07 05:41:11 -0800

    Task 3 update

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -5,8 +5,3 @@ def uniq_add(my_list=[]):
     for i in num:
         total += i
     return total
-
-
-my_list = [1, 2, 3, 1, 4, 2, 5]
-result = uniq_add(my_list)
-print("Result: {:d}".format(result))
