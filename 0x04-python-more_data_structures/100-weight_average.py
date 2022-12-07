2476d817ea3828cc37a4ae5f25685947603d4658
edmundspira <edmunddspira@gmail.com>
2022-12-07 05:23:30 -0800

    Task 14 update

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,9 +3,3 @@ def weight_average(my_list=[]):
     if my_list is None or len(my_list) == 0:
         return 0
     return (sum([x*b for x, b in my_list]) / sum([b for x, b in my_list]))
-
-
-my_list = [(1, 2), (2, 1), (3, 10), (4, 2)]
-# = ((1 * 2) + (2 * 1) + (3 * 10) + (4 * 2)) / (2 + 1 + 10 + 2)
-result = weight_average(my_list)
-print("Average: {:0.2f}".format(result))
